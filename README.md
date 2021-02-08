68d04f3990fdbbd73a1fc4e6cea06244a60d8967
Rita <ritadecassia.infor2@gmail.com>
2021-02-08 12:03:23 -0300

    Código completo, falta apenas o readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# About project:
+
+
+I understand more about manipulating the DOM and this project was made to test this knowledge. It is a music player, which serves to listen to songs already placed in the project, but nothing prevents it from being done in the future by consuming an api, I intend to test soon.
+
+# Project images: 
+
+<img src="./ImagensGithub/Track.png">
+<img src="./ImagensGithub/TrackStarted.png">
+<img src="./ImagensGithub/TrackTwo.png">
+<img src="./ImagensGithub/TrackStartedTwo.png">
---
+++ src/PlayerLogical/player.js
@@ -0,0 +1,97 @@
+var author = document.querySelector("#author");
+var music = document.querySelector("#music");
+var image = document.querySelector("#image");
+var play = document.querySelector("#play");
+var inputDuration = document.querySelector("#inputDuration");
+
+let index_no = 0;
+let time;
+let playing_song = false;
+let track = document.createElement('audio');
+
+const All_songs = [
+	{
+		track_music: "nightcall",
+		img: "image/Kavinsky.jpg",
+		path: "music/nightcall.mp3",
+		name_author: "Kavinsky"
+
+	},
+
+	{
+		track_music: "Friends",
+		img: "image/RexOrange.jpg",
+		path: "music/friends.mp3",
+		name_author: "RexOrange"
+	}
+];
+
+
+function load_track(index_no) {
+	track.src = All_songs[index_no].path;
+	track.load();
+	image.src = All_songs[index_no].img;
+	author.innerHTML = All_songs[index_no].name_author;
+	music.innerHTML = All_songs[index_no].track_music;
+	time = setInterval(RangeSlider, 1000);
+}
+
+load_track(index_no);
+
+function JustPlay() {
+	if (playing_song == false) {
+		PlaySong();
+	}else {
+		PauseSong();
+	}
+}
+
+function PlaySong() {
+	track.play();
+	playing_song = true;
+	play.innerHTML = '<i class="fas fa-pause"></i>'
+}
+
+function PauseSong() {
+	track.pause();
+	playing_song = false;
+	play.innerHTML = '<i class="fas fa-play"></i>'
+}
+
+function Next() {
+	if (index_no < All_songs.length - 1) {
+		index_no += 1;
+		load_track(index_no)
+		PlaySong();
+	}else {
+		index_no = 0;
+		load_track(index_no);
+		PlaySong();
+	}
+}
+
+function Return() {
+	if (index_no > 0) {
+		index_no -= 1;
+		load_track(index_no)
+		PlaySong();
+	}else{
+		index_no = All_songs.length;
+		load_track(index_no);
+		PlaySong();
+	}
+}
+
+function ChangeDuration() {
+	sliderPosition = track.duration * (inputDuration.value / 100);
+	track.currentTime = sliderPosition;
+}
+
+function RangeSlider() {
+	let position = 0;
+
+	if (!isNaN(track.duration)) {
+		position = track.currentTime * (100 / track.duration);
+		inputDuration.value = position;
+	}
+}
